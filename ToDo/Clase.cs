b4cbf576f193622cf507ed51f3b217364fb2384f
Bruno Cordoba <brunonahuel87@gmail.com>
2025-06-09 15:43:14 -0300

    creacion de funciones

--- ToDo/Clase.cs
+++ ToDo/Clase.cs
@@ -1,6 +1,6 @@
 using System.Dynamic;
 
-namespace Clase
+namespace MundoTarea
 {
     public class Tarea
     {
@@ -18,10 +18,10 @@ namespace Clase
                 {
                     duracion = value;
                 }
-                // Opcional: agregar un else para avisar si es inválido
+
                 else
                 {
-                    throw new ArgumentOutOfRangeException(nameof(Duracion), "La duración debe estar entre 10 y 100.");
+                    Console.WriteLine("Ingrese un numero entre 10 y 100");
                 }
 
             }
@@ -34,6 +34,38 @@ namespace Clase
             this.TareaID = tareaID;
             this.Descripcion = descripcion;
             this.Duracion = duracion; // Aquí se aplica la validación
+        }
+
+    }
+
+    class MisFunciones
+    {
+        public void BuscarDescripcion(List<Tarea> Lista, string palabraABuscar)
+        {
+
+            foreach (var tarea in Lista)
+            {
+                if (tarea.Descripcion.Contains(palabraABuscar))
+                {
+                    Console.WriteLine("Palabra encontrada");
+                }
+            }
+        }
+
+        public static void MostrarTodasLasTareas(List<Tarea> pendientes, List<Tarea> realizadas)
+        {
+            Console.WriteLine("=== TAREAS PENDIENTES ===");
+            foreach (var tarea in pendientes)
+            {
+                Console.WriteLine($"ID: {tarea.TareaID} | Descripción: {tarea.Descripcion} | Duración: {tarea.Duracion}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=== TAREAS REALIZADAS ===");
+            foreach (var tarea in realizadas)
+            {
+                Console.WriteLine($"ID: {tarea.TareaID} | Descripción: {tarea.Descripcion} | Duración: {tarea.Duracion}");
+            }
         }
 
     }
--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MundoTarea;
+Random rand = new Random();
+
+List<Tarea> TareasPendientes = new List<Tarea>();
+
+int Ntareas = 5;
+
+for (int i = 0; i < Ntareas; i++)
+{
+    Tarea Nuevatarea = new Tarea(i, $"Tarea {i}", rand.Next(10, 101));
+
+    TareasPendientes.Add(Nuevatarea);
+
+
+}
+
+/*
+Console.WriteLine("\tLista de tareas pendientes");
+foreach (var tarea in TareasPendientes)
+{
+    Console.WriteLine($"Id:{tarea.TareaID}, Descripcion: {tarea.Descripcion}, Duracion:{tarea.Duracion}");
+}
+*/
+ // Punto 3
+List<Tarea> TareasRealizadas = new List<Tarea>();
+int opcion;
+Console.WriteLine("Quiere mover las tareas pendientes a realizadas?");
+Console.WriteLine("1 = Sí, 0 = No");
+
+string entrada = Console.ReadLine();
+opcion = int.Parse(entrada);
+
+if (opcion == 1)
+{
+    foreach (var TareaPendiente in TareasPendientes)
+    {
+        TareasRealizadas.Add(TareaPendiente);
+    }
+}
+
+foreach (var tareaLista in TareasRealizadas)
+{
+    Console.WriteLine(tareaLista.Descripcion);
+    Console.WriteLine();
+}
+
+
+// punto 4 
+
+MisFunciones funcion = new MisFunciones();
+funcion.BuscarDescripcion(TareasRealizadas,"Tarea 2");
+
+
+
